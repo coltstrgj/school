daadebf26ae71d5056f8aca0b340bb6753e5a782
coltstrgj <coltstrgj@gmail.com>
2014-10-07 15:30:30 -0600

    About to mess some code up, better not lose this

--- CS370/HW2/Darien_Colt_Coordinator.c
+++ CS370/HW2/Darien_Colt_Coordinator.c
@@ -5,29 +5,75 @@
 int coordinate(char* divisor, char* dividend){
     int cpid, w;//TODO create a 2d array that stores PID Pipe, and return
     int status;
-    cpid=fork();
-    if (cpid == -1) {
-        perror("fork");
-          exit(EXIT_FAILURE);
+
+    //there will be 4 children, so we will keep a 2dimensional array where 
+    //i is the child number [i][0] is the pipe for child i and [i][1] is the return value 
+    int childTrack[4][2];
+    //for(int i=0; i<4; i++){
+    int fildes[2];
+    //childTrack[i]=fildes;
+    const int BSIZE = 100;//this will be used so that parent can send the shared memory info to child
+    char buf[BSIZE];
+    ssize_t nbytes;
+    //create the pipe that parent will use to send shared memory info to child
+    status = pipe(fildes);
+    if (status==-1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
-    if(cpid==0){
-        //is child
+    //fork the programs and decide whether the instance is the parent or child
+    //switch (fork()) {
+    //case −1: /* Handle error */
+    //    break;
+
+    //case 0:  /* Child - reads from pipe */
+    //    close(fildes[1]);                       /* Write end is unused */
+    //    nbytes = read(fildes[0], buf, BSIZE);   /* Get data from pipe */
+    //    /* At this point, a further read would see end of file ... */
+    //    close(fildes[0]);                       /* Finished with pipe */
+    //    exit(EXIT_SUCCESS);
+
+    //default:  /* Parent - writes to pipe */
+    //    close(fildes[0]);                       /* Read end is unused */
+    //    write(fildes[1], "Hello world\n", 12);  /* Write data on pipe */
+    //    close(fildes[1]);                       /* Child will see EOF */
+    //    exit(EXIT_SUCCESS);
+    //}
+    cpid=fork();
+    if (cpid == -1) {//neither parent or child
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if(cpid==0){//is child
+        //the child will get the shared memory information from the pipe 
+        close(fildes[1]);/* Write end is unused */
+        //    nbytes = read(fildes[0], buf, BSIZE);   /* Get data from pipe */
+        //    /* At this point, a further read would see end of file ... */
+        //    close(fildes[0]);                       /* Finished with pipe */
+        //    exit(EXIT_SUCCESS);
         if (-1 == execl("./Darien_Colt_Checker.out", "Darien_Colt_Checker.out", divisor, dividend, NULL)){
             perror("execlp() failed");//this means it is broken, because it should never reach this code unless it fails to execl
             return 0;
         }
-    }else{
-        //is parent
+    }else{//is parent
+        //the parent needs to create shared memory and send the info to the child
         printf("Coordinator: forked process with ID: %i\n", cpid);
-        printf("Coordinator: waiting for process [%i]\n", cpid);   
+        //default:  /* Parent - writes to pipe */
+        close(fildes[0]);                       /* Read end is unused */
+        //    write(fildes[1], "Hello world\n", 12);  /* Write data on pipe */
+        //    close(fildes[1]);                       /* Child will see EOF */
+        //    exit(EXIT_SUCCESS);
+        //printf("Coordinator: waiting for process [%i]\n", cpid);   
         w = waitpid(cpid, &status, WUNTRACED | WCONTINUED);//the following is shamelessly stolen from man pages
-            if (w == -1) {
-                perror("waitpid");
-                exit(EXIT_FAILURE);
-            }
-        printf("Coordinator: child process %i returned %i.\n", cpid,  WEXITSTATUS(status));//grabs the exit status of the previous child, and outputs it before returning to my awful "loop" in main
+        if (w == -1) {
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
+        printf("Coordinator: child process %i returned %i.\n", cpid,  WEXITSTATUS(status));
+        //grabs the exit status of the previous child, and outputs it before returning to my awful "loop" in main
         return 1;
     }
+    //}
 }
 int main(int argc, char* argv[]){
     if(argc!=6){
@@ -35,10 +81,40 @@ int main(int argc, char* argv[]){
         return 0;
     }
     //the following loop calls the method above and creates child processes
-//  for(int i=0; i<4; i++){
-//      coordinate(argv[1],argv[i+2]); 
-//  }
+    //  for(int i=0; i<4; i++){
+    //      coordinate(argv[1],argv[i+2]); 
+    //  }
 
-        coordinate(argv[1],argv[2]); 
+    coordinate(argv[1],argv[2]); 
 }
+//the following is from the man pages
+//int fildes[2];
+//const int BSIZE = 100;
+//char buf[BSIZE];
+//ssize_t nbytes;
+//int status;
+
+//status = pipe(fildes);
+//if (status == −1 ) {
+//    /* an error occurred */
+//    ...
+//}
+
+//switch (fork()) {
+//case −1: /* Handle error */
+//    break;
+
+//case 0:  /* Child - reads from pipe */
+//    close(fildes[1]);                       /* Write end is unused */
+//    nbytes = read(fildes[0], buf, BSIZE);   /* Get data from pipe */
+//    /* At this point, a further read would see end of file ... */
+//    close(fildes[0]);                       /* Finished with pipe */
+//    exit(EXIT_SUCCESS);
+
+//default:  /* Parent - writes to pipe */
+//    close(fildes[0]);                       /* Read end is unused */
+//    write(fildes[1], "Hello world\n", 12);  /* Write data on pipe */
+//    close(fildes[1]);                       /* Child will see EOF */
+//    exit(EXIT_SUCCESS);
+//}
 
--- CS370/HW2/README.txt
+++ CS370/HW2/README.txt
@@ -1,10 +1,11 @@
 Darien_Colt_Coordinator.c
     This file takes 5 arguments. the divisor, and 5 dividends.
     This file is the "parent" program. It will fork and then exec Darien_Colt_Checker
-    This program also outputs the return value from each child as well as the PID of itself and the children.
+    This program also outputs the value calculated by its child (1 represents divisible, 0 not divisible)
+    It will retrieve the output from a shared memory address which it shared over a pipe with Darien_Colt_Checker.out
 Darien_Colt_Checker.c
     This file is the source for a program that takes 2 arguments, the divisor and dividend. It will then calculate whether the dividend is divisible by the divisor
-    the file will return 0 if they are not divisible and 1 if they are.
+    the file will write 1 to a shared memory if the numbers are divisible, 0 otherwise.
 
 Makefile
-    This is the file that will allow you wo quickly compile and remove compiled code. You will not have to type gcc and the output names anymore.
+    This is the file that will allow you wo quickly compile and remove compiled code. You will not have to type gcc and the output names anymore. It uses all of the flags that I normally use.
