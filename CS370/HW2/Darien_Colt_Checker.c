460775ba799ccb56afd8cc0a4c5a1420fe5425bc
coltstrgj <coltstrgj@gmail.com>
2014-10-08 00:01:26 -0600

    working pipe and shmid, about to try attachint to shm

--- CS370/HW2/Darien_Colt_Checker.c
+++ CS370/HW2/Darien_Colt_Checker.c
@@ -27,7 +27,74 @@ int main(int argc, char* argv[]){
     /* At this point, a further read would see end of file ... */
     close(atoi(argv[1]));                       /* Finished with pipe */
     //printf("Checker process [%i]: got %s\n", getpid(), buf);
-    printf("Checker process [%i]: read %i bytes containing shm ID %s\n", getpid(), nbytes, buf);
+    int shmid=(atoi(buf));
+    printf("                                                                %i\n", shmid);
+    printf("Checker process [%i]: read %i bytes containing shm ID %i\n", getpid(), nbytes, buf);
     return 1;
-//    exit(EXIT_SUCCESS);
-} 
+    //    exit(EXIT_SUCCESS);
+}
+//--Checker process [25498]: starting.
+//
+//TODO Checker process [25498]: read 4 bytes containing shm ID 4128846
+//
+//TODO Checker process [25498]: 49 *IS NOT* divisible by 3.
+//
+//TODO Checker process [25498]: wrote result (0) to shared memory.
+
+
+//
+// shm-client - client program to demonstrate shared memory.
+///
+
+//nclude <sys/types.h>
+//nclude <sys/ipc.h>
+//nclude <sys/shm.h>
+//nclude <stdio.h>
+
+//efine SHMSZ     27
+
+//  main()
+//  {
+//      int shmid;
+//      key_t key;
+//      char *shm, *s;
+
+//      /*
+//       * We need to get the segment named
+//       * "5678", created by the server.
+//       */
+//      key = 5678;
+
+//      /*
+//       * Locate the segment.
+//       */
+//      if ((shmid = shmget(key, SHMSZ, 0666)) < 0) {
+//          perror("shmget");
+//          exit(1);
+//      }
+
+//      /*
+//       * Now we attach the segment to our data space.
+//       */
+//      if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
+//          perror("shmat");
+//          exit(1);
+//      }
+
+//      /*
+//       * Now read what the server put in the memory.
+//       */
+//      for (s = shm; *s != NULL; s++)
+//          putchar(*s);
+//      putchar('\n');
+
+//      /*
+//       * Finally, change the first character of the 
+//       * segment to '*', indicating we have read 
+//       * the segment.
+//       */
+//      *shm = '*';
+
+//      exit(0);
+//  }
+
--- CS370/HW2/Darien_Colt_Coordinator.c
+++ CS370/HW2/Darien_Colt_Coordinator.c
@@ -5,73 +5,105 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 
-int coordinate(char* divisor, char* dividend){
-    int cpid, w;//TODO create a 2d array that stores PID Pipe, and return
-    int status;
-
-    //there will be 4 children, so we will keep a 2dimensional array where 
-    //i is the child number [i][0] is the shm info for child i and [i][1] is the return value 
-    //int childTrack[4][2];//
-    //for(int i=0; i<4; i++){
-    int fildes[2];
-    //childTrack[i]=fildes;
-    //create the pipe that parent will use to send shared memory info to child
-    status = pipe(fildes);
-    if (status==-1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-    //////////////begin the fork
-    cpid=fork();
-    if (cpid == -1) {//neither parent or child
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    if(cpid==0){
-        /* Child - reads from pipe */
-        close(fildes[1]);/* Write end is unused */
-        char pipeName[BUFSIZ];
-        snprintf(pipeName, sizeof(pipeName), "%i", fildes[0]);
-        if (-1 == execl("./Darien_Colt_Checker.out", "Darien_Colt_Checker.out", pipeName, divisor, dividend, NULL)){
-            perror("execlp() failed");//this means it is broken, because it should never reach this code unless it fails to execl
-            return 0;
-        }
-    }else{//is parent
-        //the parent needs to create shared memory and send the info to the child
-        printf("Coordinator: forked process with ID: %i\n", cpid);
-        //////////create the shared memory segment
-        key_t key; /* key to be passed to shmget() */ 
-        int shmflg; /* shmflg to be passed to shmget() */ 
-        int shmid; /* return value from shmget() */ 
-        int size; /* size to be passed to shmget() */  
-        int shmget(key_t key, size_t size, int shmflg);
-        /* Parent - writes to pipe */
-        close(fildes[0]);                       /* Read end is unused */
-        int nbytes = write(fildes[1], "Hel", 4);    /* Write data on pipe */
-        close(fildes[1]);                       /* Child will see EOF */
-        printf("Coordinator: wrote shm ID %i to pipe (%i bytes)\n", 123,nbytes);
-        //    exit(EXIT_SUCCESS);
-        //printf("Coordinator: waiting for process [%i]\n", cpid);   
-        w = waitpid(cpid, &status, WUNTRACED | WCONTINUED);//the following is shamelessly stolen from man pages
-        if (w == -1) {
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-        printf("Coordinator: child process %i returned %i.\n", cpid,  WEXITSTATUS(status));
-        //grabs the exit status of the previous child, and outputs it before returning to my awful "loop" in main
-        return 1;
-    }
-    //}
-}
 int main(int argc, char* argv[]){
     if(argc!=6){
         printf("Bad input:\nUsage: 'coordinator divisor dividend dividend dividend dividend\n");
         return 0;
     }
-    //the following loop calls the method above and creates child processes
-    for(int i=0; i<4; i++){
-        coordinate(argv[1],argv[i+2]); 
-    }
+    //..array with PID of child, shm info, return value
 
-    //coordinate(argv[1],argv[2]); 
+    for(int i=0; i<4; i++){
+        ///..fork
+        int cpid, w;//TODO create a 2d array that stores PID Pipe, and return
+        int status;
+        int fildes[2];
+        //actual work from here down
+        status = pipe(fildes);
+        if (status==-1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+        cpid=fork();
+        if (cpid == -1) {//neither parent or child
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        if(cpid==0){
+            /* Child - reads from pipe */
+            close(fildes[1]);/* Write end is unused */
+            char pipeName[BUFSIZ];
+            snprintf(pipeName, sizeof(pipeName), "%i", fildes[0]);
+            //printf("fildes: %i, pipeName: %s\n\n", fildes[0], fildes);
+            if (-1 == execl("./Darien_Colt_Checker.out", "Darien_Colt_Checker.out", pipeName, argv[1], argv[i+2], NULL)){
+                perror("execlp() failed");//this means it is broken, because it should never reach this code unless it fails to execl
+                return 0;
+            }
+        }else{//is parent
+            //the parent needs to create shared memory and send the info to the child
+            printf("Coordinator: forked process with ID: %i\n", cpid);
+            //..Should create shared memory 
+            key_t key = i;                    /* key to be passed to shmget() */
+            /*"child" and i are added to get a base key because I have no idea what I should actually do here*/ 
+            int shmflg;                                 /* shmflg to be passed to shmget() */ 
+            int shmid;                                  /* return value from shmget() */ 
+            int size;                                   /* size to be passed to shmget() */  
+            shmid = shmget(key, 4, IPC_CREAT | 0644);
+            ///////printf("shmflg:%i size:%i shmid:%i \n",shmflg, size, shmid);
+            //..Add relevant info to shared mem array TODO
+            //..create pipe and send it relevant info on shared memory TODO 
+            /* Parent - writes to pipe */
+            //actually creates the pipe
+
+            char shm_name[BUFSIZ];
+            snprintf(shm_name, sizeof(shm_name), "%i %i", shmid, i);
+            close(fildes[0]);                           /* Read end is unused */
+            int nbytes = write(fildes[1], shm_name, 6);    /* Write data on pipe */
+            close(fildes[1]);                           /* Child will see EOF */
+            printf("Coordinator: wrote shm ID %i to pipe (%i bytes)\n", shmid,nbytes);
+            //Coordinator: wrote shm ID 4063308 to pipe (4 bytes) TODO
+
+        }
+
+    }
+    /*
+       for(int i=0; i<4; i++){
+//..wait for the child TODO
+int w, status;
+////////////wait for the cpid stored in the array
+w = waitpid(cpid, &status, WUNTRACED | WCONTINUED);
+if (w == -1) {
+perror("waitpid");
+exit(EXIT_FAILURE);
 }
+printf("Coordinator: child process %i returned %i.\n", cpid,  WEXITSTATUS(status));
+//grabs the exit status of the previous child, and outputs it before returning to my awful "loop" in main
+return 1;
+//..read the info from shared memory TODO
+}*/
+while(0<1){;}
+return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
